e2c66b73505cc9a8d8381d86a5e0765eeae6b90e
dbsxdbsx <djw0214@gmail.com>
2023-10-07 10:28:46 +0800

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 mod test_custom_type_loop;
 
-fn main() {}
+struct MyStruct {
+    rust_output_path: String,
+}
+impl MyStruct {
+    pub fn get_complex(&self) -> Vec<usize> {
+        println!("======================");
+        vec![1, 2, 3, 4]
+    }
+}
+
+fn test() {
+    panic!("test");
+}
+fn main() {
+    test();
+    let op = Some(MyStruct {
+        rust_output_path: "".to_string(),
+    });
+    let r = op.unwrap();
+
+    r.get_complex().iter().for_each(|each| {
+        println!("{:?}", each);
+    });
+}
